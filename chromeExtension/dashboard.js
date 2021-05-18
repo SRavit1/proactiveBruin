70f495c72abc74f6e92b51b9a4df7a310a3cd0ae
Emmettlsc <83787788+Emmettlsc@users.noreply.github.com>
2021-05-18 16:01:36 -0400

    Changing charts via dropdown and Categorizing Chart

--- chromeExtension/dashboard.js
+++ chromeExtension/dashboard.js
@@ -1,5 +1,11 @@
 background_window = chrome.extension.getBackgroundPage().window
 
+//keeps stores site data and cleaned data for global use
+var globsiteData 
+var globProcessedSiteData
+var extensionid = chrome.runtime.id	//32 char id given to each extension 
+
+
 function listSiteData(siteData)
 {
 
@@ -29,7 +35,9 @@ function listSiteData(siteData)
 	screentimeData.sort(function(a,b){
 		return a[1]-b[1]
 	});
-
+	
+	globProcessedSiteData = screentimeData //sets processed var on load
+	
 	return screentimeData;
 }
 
@@ -69,11 +77,95 @@ function onLoad() {
 			siteData = JSON.parse(siteDataText)["key_val"]
 
 			console.log(siteData)
-
+			
+			globsiteData = siteData //sets glob var on load
+			
 			google.charts.setOnLoadCallback(drawChart(siteData));
 			done = true
 		}
 	}
-
-	
 }
+
+document.addEventListener('DOMContentLoaded', function(){
+const selectElement = document.querySelector('.statsselect');
+selectElement.addEventListener('change', (event) => {
+  const result = document.getElementById('result');
+  result.textContent = `You selected ${event.target.value}`;
+  if(event.target.value == 'Websites'){
+  	google.charts.setOnLoadCallback(drawChart(globsiteData));
+  }
+  else {
+  	drawCategoryChart()
+  }
+});
+})
+
+
+function drawCategoryChart() {
+	var entertainmentnum = 0
+	var productivitynum = 0
+	var other = 0
+	var shoppingnum = 0
+	for (i=1; i<globProcessedSiteData.length; i++){
+		temp = globProcessedSiteData[i][0]
+
+		if(Entertainment.includes(temp)){
+			entertainmentnum+=Number(globProcessedSiteData[i][1])
+		}
+		else if(Productivity.includes(temp)){
+			productivitynum+=Number(globProcessedSiteData[i][1])
+		}
+		else if(Shopping.includes(temp)){
+			shoppingnum+=Number(globProcessedSiteData[i][1])
+		}
+		else{
+			othernum+=Number(globProcessedSiteData[i][1])
+		}
+	}
+	var data = google.visualization.arrayToDataTable([
+	  ['Task', 'Hours per Day'],
+	  ['Entertainment',     entertainmentnum],
+	  ['Productivity',      productivitynum],
+	  ['Shopping',  shoppingnum],
+	  ['Other',    othernum],
+	]);
+
+	var options = {
+	  title: 'Daily Screentime By Category'
+	};
+
+	var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+	chart.draw(data, options);
+}
+
+
+
+//websites in each category
+var Entertainment =[
+"www.youtube.com",
+"www.netflix.com",
+"www.hulu.com",
+"www.twitch.tv",
+"www.disneyplus.com",
+"www.primeivideo.com",
+"www.xfinity.com",
+"www.hbomax.com"
+]
+
+var Productivity = [
+"accounts.google.com",
+"drive.google.com",
+"ccle.ucla.edu",
+"mail.google.com",
+"www.google.com",
+"stackoverflow.com",
+"libgen.is",
+"extensions",
+"ProactiveBruin"
+]
+
+var Shopping = [
+"www.target.com",
+"www.amazon.com"
+]
