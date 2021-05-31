267b2d0331481bbaaea3f8226948578ee9731cc4
Ravit Sharma <sharma.ravit@gmail.com>
2021-05-31 08:46:20 -0700

    Combining master script into web server.

--- webServer/Server.js
+++ webServer/Server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 var sync_mysql = require('sync-mysql')
 var utils = require('./utils.js');
 const app = express();
-
+var assert = require('assert');
 
 var knownIds = []
 
@@ -14,6 +14,11 @@ var sync_con = new sync_mysql({
   database: "proactiveBruinData",
 })
 console.log("Successfully connected to mysql server!");
+
+var PORT = 3000
+app.listen(PORT, function() {
+    console.log("Listening on port " + PORT)
+});
 
 app.use(bodyParser.json({ extended: true }));
 
@@ -192,11 +197,6 @@ app.post('/requestGoalData', function (req, res) {
 	res.end()
 })
 
-var PORT = 3000
-app.listen(PORT, function() {
-    console.log("Listening on port " + PORT)
-});
-
 app.post('/requestCatData', function (req, res) {
 	console.log("requestCatData request to server")
 
@@ -297,3 +297,39 @@ var Shopping = [
 "www.amazon.com",
 "www.urbanoutfitters.com"
 ]
+
+//Master script: Run every day
+
+function updateGoalEntries (ID, date) {
+	let goalQuery = "SELECT * FROM " + ID + "_goal WHERE date=\"" + date + "\";"
+	console.log(goalQuery)
+	let goalQueryRes = sync_con.query(goalQuery)
+	for (let i = 0; i < goalQueryRes.length; i++) {
+		let dataQuery = "SELECT * FROM " + ID + " WHERE date=\"" + date + "\"" + " AND " + "hostname=\"" + goalQueryRes[i].hostname + "\";"
+		console.log(dataQuery)
+		let dataQueryRes = sync_con.query(dataQuery)
+		assert(dataQueryRes.length == 0 | dataQueryRes.length == 1)
+		if (dataQueryRes.length == 1) {
+			let timeSpent = dataQueryRes[0].time
+
+			let updateQuery = "UPDATE " + ID + "_goal SET timeSpent=" + timeSpent + " WHERE date=\"" + date + "\" AND hostname=\"" + goalQueryRes[i].hostname + "\";";
+			console.log(updateQuery)
+			sync_con.query(updateQuery)
+
+		}
+	}
+}
+
+function updateGoal() {
+	let currDate = utils.getCurrDateString()
+
+	tables = sync_con.query("SHOW TABLES;")
+	for (let i = 0; i < tables.length; i++) {
+		tableName = Object.values(tables[i])[0]
+		if (/^[a-z]+$/.test(tableName)) { //Theoretically, condition is true iff tableName is a user id
+			updateGoalEntries(tableName, currDate)
+		}
+	}
+}
+
+setInterval(updateGoal, 1000*60*60*24) //call updateGoal on a daily basis
